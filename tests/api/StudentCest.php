b803b228e512d87c4f33150b9acd9546aa8c6eb4
sebastien <sebastien.caussidery@abbeal.com>
2021-01-12 13:56:39 +0100

    add codeception and create first test

---
+++ tests/api/StudentCest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Tests\api;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class StudentCest
+{
+    public function _before(\ApiTester $I)
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+    }
+
+    public function _after(\ApiTester $I)
+    {
+    }
+
+    public function testListStudent(\ApiTester $I) {
+
+        $I->wantTo('Check this list of student');
+        $I->sendGET('/api/students');
+        $I->seeResponseIsJson();
+        $I->seeResponseCodeIs(Response::HTTP_OK);
+    }
+}
